0ddf6e06ba19d8e254583b4875b4e8d08f73d16b
Jangseohyun <jangseohyun@yonsei.ac.kr>
2025-04-01 10:34:21 +0900

    04011034

--- IIE4122/ga.py
+++ IIE4122/ga.py
@@ -16,3 +16,255 @@ end while
 Decode the results and visualizations
 end
 '''
+
+# Traveling Salesman Problem using Genetic Algorithm
+
+from itertools import permutations
+from random import shuffle
+import random
+import numpy as np
+import pandas as pd
+
+def initial_population(cities_list, n_population = 250):
+    
+    """
+    Generating initial population of cities randomly selected from all 
+    possible permutations  of the given cities
+    Input:
+    1- Cities list 
+    2. Number of population 
+    Output:
+    Generated lists of cities
+    """
+    
+    population_perms = []
+    possible_perms = list(permutations(cities_list))
+    random_ids = random.sample(range(0,len(possible_perms)),n_population)
+    for i in random_ids:
+        population_perms.append(list(possible_perms[i]))
+        
+    return population_perms
+
+def dist_two_cities(city_1, city_2):
+    
+    """
+    Calculating the distance between two cities  
+    Input:
+    1- City one name 
+    2- City two name
+    Output:
+    Calculated Euclidean distance between two cities
+    """
+    
+    city_1_coords = city_coords[city_1]
+    city_2_coords = city_coords[city_2]
+    return np.sqrt(np.sum((np.array(city_1_coords) - np.array(city_2_coords))**2))
+
+def total_dist_individual(individual):
+    
+    """
+    Calculating the total distance traveled by individual, 
+    one individual means one possible solution (1 permutation)
+    Input:
+    1- Individual list of cities 
+    Output:
+    Total distance traveled 
+    """
+    
+    total_dist = 0
+    for i in range(0, len(individual)):
+        if(i == len(individual) - 1):
+            total_dist += dist_two_cities(individual[i], individual[0])
+        else:
+            total_dist += dist_two_cities(individual[i], individual[i+1])
+    return total_dist
+
+def fitness_prob(population):
+    """
+    Calculating the fitness probability 
+    Input:
+    1- Population  
+    Output:
+    Population fitness probability 
+    """
+    total_dist_all_individuals = []
+    for i in range (0, len(population)):
+            total_dist_all_individuals.append(total_dist_individual(population[i]))
+            
+    max_population_cost = max(total_dist_all_individuals)
+    population_fitness = max_population_cost - total_dist_all_individuals
+    population_fitness_sum = sum(population_fitness)
+    population_fitness_probs = population_fitness / population_fitness_sum
+    return population_fitness_probs
+
+def roulette_wheel(population, fitness_probs):
+    """
+    Implement a selection strategy based on proportionate roulette wheel
+    Selection.
+    Input:
+    1- population
+    2: fitness probabilities 
+    Output:
+    selected individual
+    """
+    population_fitness_probs_cumsum = fitness_probs.cumsum()
+    bool_prob_array = population_fitness_probs_cumsum < np.random.uniform(0,1,1)
+    selected_individual_index = len(bool_prob_array[bool_prob_array == True]) - 1
+    return population[selected_individual_index]
+
+def crossover(parent_1, parent_2):
+    """
+    Implement mating strategy using simple crossover between two parents
+    Input:
+    1- parent 1
+    2- parent 2 
+    Output:
+    1- offspring 1
+    2- offspring 2
+    """
+    n_cities_cut = len(cities_names) - 1
+    cut = round(random.uniform(1, n_cities_cut))
+    offspring_1 = []
+    offspring_2 = []
+    
+    offspring_1 = parent_1 [0:cut]
+    offspring_1 += [city for city in parent_2 if city not in offspring_1]
+    
+    offspring_2 = parent_2 [0:cut]
+    offspring_2 += [city for city in parent_1 if city not in offspring_2]
+    
+    
+    return offspring_1, offspring_2
+
+def mutation(offspring):
+    """
+    Implement mutation strategy in a single offspring
+    Input:
+    1- offspring individual
+    Output:
+    1- mutated offspring individual
+    """
+    n_cities_cut = len(cities_names) - 1
+    index_1 = round(random.uniform(0,n_cities_cut))
+    index_2 = round(random.uniform(0,n_cities_cut))
+
+    temp = offspring [index_1]
+    offspring[index_1] = offspring[index_2]
+    offspring[index_2] = temp
+    return(offspring)
+
+def run_ga(cities_names, n_population, n_generations,
+           crossover_per, mutation_per):
+    
+    population = initial_population(cities_names, n_population)
+    fitness_probs = fitness_prob(population)
+    
+    parents_list = []
+    for i in range(0, int(crossover_per * n_population)):
+        parents_list.append(roulette_wheel(population,
+                                           fitness_probs))
+
+    offspring_list = []    
+    for i in range(0,len(parents_list), 2):
+        offspring_1, offspring_2 = crossover(parents_list[i], 
+                                             parents_list[i+1])
+
+    #     print(offspring_1)
+    #     print(offspring_2)
+    #     print()
+
+        mutate_threashold = random.random()
+        if(mutate_threashold > (1-mutation_per)):
+            offspring_1 = mutation(offspring_1)
+    #         print("Offspring 1 mutated", offspring_1)
+
+        mutate_threashold = random.random()
+        if(mutate_threashold > (1-mutation_per)):
+            offspring_2 = mutation(offspring_2)
+    #         print("Offspring 2 mutated", offspring_2)
+
+
+        offspring_list.append(offspring_1)
+        offspring_list.append(offspring_2)
+
+    mixed_offspring = parents_list + offspring_list
+
+    fitness_probs = fitness_prob(mixed_offspring)
+    sorted_fitness_indices = np.argsort(fitness_probs)[::-1]
+    best_fitness_indices = sorted_fitness_indices[0:n_population]
+    best_mixed_offsrping = []
+    for i in best_fitness_indices:
+        best_mixed_offsrping.append(mixed_offspring[i])
+
+
+    for i in range(0, n_generations):
+        # if (i%10 == 0):
+            # print("Generation: ", i)
+        
+        fitness_probs = fitness_prob(best_mixed_offsrping)
+        parents_list = []
+        for i in range(0, int(crossover_per * n_population)):
+            parents_list.append(roulette_wheel(best_mixed_offsrping, 
+                                               fitness_probs))
+
+        offspring_list = []    
+        for i in range(0,len(parents_list), 2):
+            offspring_1, offspring_2 = crossover(parents_list[i], 
+                                                 parents_list[i+1])
+
+            mutate_threashold = random.random()
+            if(mutate_threashold > (1-mutation_per)):
+                offspring_1 = mutation(offspring_1)
+
+            mutate_threashold = random.random()
+            if(mutate_threashold > (1-mutation_per)):
+                offspring_2 = mutation(offspring_2)
+
+            offspring_list.append(offspring_1)
+            offspring_list.append(offspring_2)
+
+
+        mixed_offspring = parents_list + offspring_list
+        fitness_probs = fitness_prob(mixed_offspring)
+        sorted_fitness_indices = np.argsort(fitness_probs)[::-1]
+        best_fitness_indices = sorted_fitness_indices[0:int(0.8*n_population)]
+
+        best_mixed_offsrping = []
+        for i in best_fitness_indices:
+            best_mixed_offsrping.append(mixed_offspring[i])
+            
+        old_population_indices = [random.randint(0, (n_population - 1)) 
+                                  for j in range(int(0.2*n_population))]
+        for i in old_population_indices:
+#             print(i)
+            best_mixed_offsrping.append(population[i])
+            
+        random.shuffle(best_mixed_offsrping)
+            
+    return best_mixed_offsrping
+
+
+n_population = 250
+crossover_per = 0.8
+mutation_per = 0.2
+n_generations = 200
+
+x = [0,3,6,7,15,10,16,5,8,1.5]
+y = [1,2,1,4.5,-1,2.5,11,6,9,12]
+cities_names = ["Gliwice", "Cairo", "Rome", "Krakow", "Paris", 
+                "Alexandria", "Berlin", "Tokyo", "Hong Kong", "Rio"]
+city_coords = dict(zip(cities_names, zip(x, y)))
+
+best_mixed_offsrping = run_ga(cities_names, n_population,
+                              n_generations, crossover_per, mutation_per)
+
+total_dist_all_individuals = []
+for i in range(0, n_population):
+    total_dist_all_individuals.append(total_dist_individual(best_mixed_offsrping[i]))
+
+index_minimum = np.argmin(total_dist_all_individuals)
+minimum_distance = min(total_dist_all_individuals)
+
+shortest_path = best_mixed_offsrping[index_minimum]
+
+print(minimum_distance)
